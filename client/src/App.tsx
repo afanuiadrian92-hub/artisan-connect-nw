f4045f0c034a082e4fed373cd11a1e150409f00a
Adrian <afanuiadrian92@gmail.com>
2026-04-24 16:54:26 +0100

    upgrade to TypeScript, clean App.tsx starter

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Artisan Connect NW</h1>
+      <p>Platform loading...</p>
+    </div>
+  )
+}
+
+export default App
